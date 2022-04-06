35a02cc20938902e4dc0cbbbe55d444a44cf3824
Dedo-unuka <dzanazovko@gmail.com>
2022-04-06 16:57:42 +0200

    Adding ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,18 @@
+// Create school object here
+var school = {
+    "name": "Arena",
+    "location": "Sarajevo",
+    "established": 2020
+};
+// End of school object 
+
+// Create a function below this line
+function myFunction(name) {
+    school.name = name;
+    return school;
+}
+// End of function
+myFunction("Paragon");
+console.log(myFunction("Paragon"));
+
+module.exports = {school, myFunction};
