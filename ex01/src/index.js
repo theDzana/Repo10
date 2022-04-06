f47fef6580fecd679b6c2f13c3084236e1b0c309
Dedo-unuka <dzanazovko@gmail.com>
2022-04-06 15:12:13 +0200

    Adding ex01

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+"species": "Doberman",
+"name": "Arta",
+"legs": 4,
+"friends": ["Johnny", "Teo"],
+};
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = {myPet, myFunction};
