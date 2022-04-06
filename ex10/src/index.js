08e0b55b3a9b8b8e2824da990e884d8099b15391
Dedo-unuka <dzanazovko@gmail.com>
2022-04-06 20:23:22 +0200

    Adding ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,34 @@
+function myFunction() {
+    var myMusic = {
+        001: {
+            artist: "Billy Joel",
+            title: "Piano Man",
+            relase_year: 1973,
+            formats: {
+                1: "CD",
+                2: "8T",
+                3: "LP"
+            },
+            gold: true
+        },
+        002: {
+            // Add a record here
+            artist: "Red Hot Chili Peppers",
+            title: "Californication",
+            relase_year: 1999,
+            id: 100,
+            formats: {
+                1: "CD",
+                2: "Vinyl",
+                3: "LP"
+            },
+            gold: true
+        },
+    };
+    return myMusic;
+}
+myFunction()[2];
+myFunction()[1];
+console.log(myFunction()[2]);
+console.log(myFunction()[1]);
+module.exports = myFunction;
