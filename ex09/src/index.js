510ddb72da19ab5a77820cccbee6b8c4ca06c7c4
Dedo-unuka <dzanazovko@gmail.com>
2022-04-06 22:47:46 +0200

    Adding ex09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,16 +1,20 @@
-function myFunction(myObj, checkProp) {
-// Only change code below this line
+var myObj = {
+    title: "Titanic",
+    song: "My Heart Will Go On",
+    genre: "drama"
+    };
+    function myFunction(myObj, checkProp) {
+    // Only change code below this line
 
  if (myObj.hasOwnProperty(checkProp)) {
         return myObj[checkProp];
     } 
      return "Not Found";
+     // Only change code above this line
     }
-    console.log( myFunction({title:"Titanic", song: "My heart Will Go On", genre: "drama"}, "title"));
-    console.log( myFunction({title:"Titanic", song: "My heart Will Go On", genre: "drama"}, "song"));
-    console.log( myFunction({title:"Titanic", song: "My heart Will Go On", genre: "drama"}, "genre"));
-    console.log(  myFunction({title:"Titanic", song: "My heart Will Go On", genre: "drama"}, "actor"));
-// Only change code above this line
-
-
-module.exports = myFunction;
+    console.log(myFunction(myObj,"title"));
+    console.log(myFunction(myObj,"song"));
+    console.log(myFunction(myObj,"genre"));
+    console.log(myFunction(myObj,"actor"));
+   
+  module.exports = myFunction;
